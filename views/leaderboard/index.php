fbb0cf9567528b08a4071adb8678c23076c787d6
heal25 <natkay.robert@nrcode.com>
2014-06-01 19:37:26 +0200

    improve readability

--- views/leaderboard/index.php
+++ views/leaderboard/index.php
@@ -16,7 +16,7 @@ $this->pageTitle = 'Ranglista';
         <div data-role="collapsible" data-inset="false"<?= $board->boardType == 'board_p'?' data-collapsed="false"':'';?>>
             <h2>Játékosok</h2>
             <ul data-role="listview">
-                <li><?= CHtml::link('aktuális hónap', ['index'], ['class'=>$board->boardType == 'board_p' & $board->range == 'actual'?'ui-disabled':'']); ?></li>
+                <li><?= CHtml::link('aktuális hónap', ['index'], ['class'=>$board->boardType == 'board_p' & $board->range == 'actual' ? 'ui-disabled' : '']); ?></li>
                 <li><?= CHtml::link('előző hónap', ['index', 'range'=>'prev'], ['class'=>$board->boardType == 'board_p' & $board->range == 'prev'?'ui-disabled':'']); ?></li>
                 <li><?= CHtml::link('összesített: utolsó 6 hónap', ['index', 'range'=>'last'], ['class'=>$board->boardType == 'board_p' & $board->range == 'last'?'ui-disabled':'']); ?></li>
             </ul>
@@ -24,7 +24,7 @@ $this->pageTitle = 'Ranglista';
         <div data-role="collapsible" data-inset="false"<?= $board->boardType == 'board_c'?' data-collapsed="false"':'';?>>
             <h2>Klubok</h2>
             <ul data-role="listview">
-                <li><?= CHtml::link('aktuális hónap', ['club'], ['class'=>$board->boardType == 'board_c' & $board->range == 'actual'?'ui-disabled':'']); ?></li>
+                <li><?= CHtml::link('aktuális hónap', ['club'], ['class'=>$board->boardType == 'board_c' & $board->range == 'actual' ? 'ui-disabled' : '']); ?></li>
                 <li><?= CHtml::link('előző hónap', ['club', 'range'=>'prev'], ['class'=>$board->boardType == 'board_c' & $board->range == 'prev'?'ui-disabled':'']); ?></li>
                 <li><?= CHtml::link('összesített: utolsó 6 hónap', ['club', 'range'=>'last'], ['class'=>$board->boardType == 'board_c' & $board->range == 'last'?'ui-disabled':'']); ?></li>
             </ul>
