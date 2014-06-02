2a4be705839d66b39fff039b803244d06bf3a200
heal25 <natkay.robert@nrcode.com>
2014-06-02 22:51:06 +0200

    property annotations

--- components/BadgeComponent.php
+++ components/BadgeComponent.php
@@ -1,16 +1,18 @@
 <?php
+/**
+ * @property BadgeActivator $model
+ */
 class BadgeComponent extends CApplicationComponent
 {
     private $_model;
+    
+    public function getModel() {
+        return $this->_model;
+    }
 
     public function init() {
         //echo __FUNCTION__."\n";
         $this->_model = new BadgeActivator();
         $this->_model->uid = Yii::app()->player->uid;
-    }
-
-    public function getModel() {
-        return $this->_model;
-    }
+    }    
 }
-
