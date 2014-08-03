a829d6adb010936b3348d6264f070a55bdf44718
heal25 <natkay.robert@nrcode.com>
2014-08-03 20:15:19 +0200

    set debug

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DEBUG') || define('YII_DEBUG', false);
-defined('YII_TRACE_LEVEL') || define('YII_TRACE_LEVEL',3);
+defined('YII_DEBUG') || define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') || define('YII_TRACE_LEVEL', 3);
 
 // change the following paths if necessary
 require(dirname(__FILE__) . '/../../yii/framework/yii.php');
