8fe11ea62d6d3afde4be046407c8eee180004588
heal25 <natkay.robert@nrcode.com>
2014-08-22 12:52:42 +0200

    credits imamge

--- views/site/credits.php
+++ views/site/credits.php
@@ -7,7 +7,10 @@ $this->pageTitle = 'Készítők';
     <h1>Készítők</h1>
 </div>
 
-<p>A játék készítője: nrcode s.r.o.</p>
+<div class="credits-grid ui-grid-a">
+    <div class="ui-block-a"><?= CHtml::image('/images/me.png', 'A fejlesztő'); ?></div>
+    <div class="ui-block-b">A játék készítője:<br/>nrcode s.r.o.<br/>Natkay Róbert</div>
+</div>
 
 <ul data-role="listview" data-inset="true">
     <li data-role="list-divider">Akik nélkül a játék nem létezne</li>
