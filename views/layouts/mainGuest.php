7ca220afddda34837db14b09ef7c361290579a21
Robert Natkay <robert@nrcode.com>
2016-08-27 20:51:16 +0200

    whitespaces

--- views/layouts/mainGuest.php
+++ views/layouts/mainGuest.php
@@ -2,14 +2,14 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
     <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
 </head>
 <body>
 
-    <div data-role="page" id="index">    
-        
+    <div data-role="page" id="index">
+
         <div data-role="header" id="game-header">
             <?= CHtml::link('Navigáció', ['#left-panel'], ['id'=>'nav', 'data-icon'=>'bars', 'data-iconpos'=>'notext']); ?>
             <div id="attributes" class="attributes">
@@ -22,18 +22,18 @@
                 foreach(Yii::app()->user->getFlashes() as $key => $message) {
                     echo '<div class="alert alert-' . $key . '">' . $message . "</div>\n";
                 }
-                echo $content; 
+                echo $content;
             ?>
         </div><!-- content -->
-    
+
         <div data-role="panel" id="left-panel">
             <ul data-role="listview">
-                <li><?= CHtml::link('Főoldal', ['/']); ?></li>
+                <li><?= CHtml::link('Főoldal', '/'); ?></li>
                 <li><?= CHtml::link('Regisztráció', ['/account/signup']); ?></li>
                 <li><?= CHtml::link('Elfelejtett jelszó', ['/account/resetPassword']); ?></li>
             </ul>
         </div><!-- /panel -->
-        
+
     </div><!-- page -->
 
 </body>
